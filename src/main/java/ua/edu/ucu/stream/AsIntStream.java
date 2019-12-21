500c7546015ee4682084317b5c94a3b50859e995
Sophia <kravchuk@ucu.edu.ua>
2019-12-21 18:48:48 +0200

    "initial Commit"

--- src/main/java/ua/edu/ucu/stream/AsIntStream.java
+++ src/main/java/ua/edu/ucu/stream/AsIntStream.java
@@ -1,70 +1,244 @@
 package ua.edu.ucu.stream;
 
+import ua.edu.ucu.exception.TheStreamIsClosed;
 import ua.edu.ucu.function.*;
+import ua.edu.ucu.iterator.Queue;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class AsIntStream implements IntStream {
+    private Iterator<Integer> streamItr;
+    private Queue<Integer> q;
+    private boolean closed = false;
 
-    private AsIntStream() {
-        // To Do
+    private AsIntStream(int... values) {
+        int n = values.length;
+        Integer[] val = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            val[i] = values[i];
+        }
+        q = new Queue<>(val);
+        streamItr = q.iterator();
+    }
+    private AsIntStream(Iterator<Integer> itr) {
+        this.streamItr = itr;
     }
 
     public static IntStream of(int... values) {
-        return null;
+        return new AsIntStream(values);
     }
 
+    private void checkEmptiness() {
+        if (!streamItr.hasNext()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkTerminated() {
+        if (closed) {
+            throw new TheStreamIsClosed();
+        }
+    }
+
+    //To change body of generated methods, choose Tools | Templates.
     @Override
     public Double average() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        checkEmptiness();
+        int sumOfEls = 0;
+        int n = 0;
+        while (streamItr.hasNext()) {
+            sumOfEls += streamItr.next();
+            n++;
+        }
+        closed = true;
+        return (double) (sumOfEls / n);
     }
 
     @Override
     public Integer max() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        checkEmptiness();
+        int firstEl = streamItr.next();
+        IntBinaryOperator op = new IntBinaryOperator() {
+            @Override
+            public int apply(int left, int right) {
+                return Math.max(left, right);
+            }
+        };
+        return reduce(firstEl, op);
     }
 
     @Override
     public Integer min() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        checkEmptiness();
+        int firstEl = streamItr.next();
+        IntBinaryOperator op = new IntBinaryOperator() {
+            @Override
+            public int apply(int left, int right) {
+                return Math.min(left, right);
+            }
+        };
+        return reduce(firstEl, op);
     }
 
     @Override
     public long count() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        long size = 0;
+        while (streamItr.hasNext()) {
+            size ++;
+            streamItr.next();
+        }
+        closed = true;
+        return size;
     }
 
     @Override
     public Integer sum() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        checkEmptiness();
+        int sumOfEls = 0;
+        while (streamItr.hasNext()) {
+            sumOfEls += streamItr.next();
+        }
+        closed = true;
+        return sumOfEls;
     }
 
     @Override
     public IntStream filter(IntPredicate predicate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        Iterator <Integer> iter = this.streamItr;
+        return new AsIntStream(new Iterator<Integer>() {
+            private Integer nextFiltered = null;
+            @Override
+            public boolean hasNext() {
+                if (nextFiltered != null) {
+                    return true;
+                }
+                while (iter.hasNext()) {
+                    Integer prev = iter.next();
+                    if (predicate.test(prev)) {
+                        nextFiltered = prev;
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public Integer next() {
+                if (nextFiltered == null) {
+                    if (!hasNext()) {
+                        throw new NoSuchElementException();
+                    }
+                }
+                Integer res = nextFiltered;
+                nextFiltered = null;
+                return res;
+            }
+        });
     }
 
     @Override
     public void forEach(IntConsumer action) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        while (streamItr.hasNext()) {
+            action.accept(streamItr.next());
+        }
+        closed = true;
     }
 
     @Override
     public IntStream map(IntUnaryOperator mapper) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        Iterator <Integer> iter = this.streamItr;
+        return new AsIntStream(new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return iter.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return mapper.apply(iter.next());
+            }
+        });
     }
 
     @Override
     public IntStream flatMap(IntToIntStreamFunction func) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+
+        Iterator<Integer> iter = this.streamItr;
+
+        return new AsIntStream(new Iterator<Integer>() {
+            private Iterator<Integer> tempItr = new Queue<Integer>().iterator();
+            @Override
+            public boolean hasNext() {
+                if (!tempItr.hasNext()) {
+                    return iter.hasNext();
+                }
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (tempItr.hasNext()) {
+                    return tempItr.next();
+                }
+                Queue<Integer> q1 = new Queue<>();
+                IntStream temp = func.applyAsIntStream(iter.next());
+                int[] tempArr = temp.toArray();
+                for (int el : tempArr) {
+                    q1.enqueue(el);
+                }
+                tempItr = q1.iterator();
+
+                return tempItr.next();
+            }
+        });
     }
 
     @Override
     public int reduce(int identity, IntBinaryOperator op) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        if (!streamItr.hasNext()) {
+            return identity;
+        }
+        int res = op.apply(identity, streamItr.next());
+        while (streamItr.hasNext()) {
+            res = op.apply(res, streamItr.next());
+        }
+        closed = true;
+        return res;
     }
 
     @Override
     public int[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        checkTerminated();
+        ArrayList<Integer> tempList = new ArrayList<>();
+        while (streamItr.hasNext()) {
+            tempList.add(streamItr.next());
+        }
+        closed = true;
+        int n = tempList.size();
+        int[] val = new int[n];
+        for (int i = 0; i < n; i++) {
+            val[i] = tempList.get(i);
+        }
+        return val;
     }
+
 
 }
